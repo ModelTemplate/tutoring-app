a2bf7b4888ecfc590e98f5951066ae10789e5324
Thomas <thmsphung2000@gmail.com>
2020-11-09 13:40:47 -0800

    Models created

---
+++ tutoring-app/Models/Schedule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tutoring_app.Models
+{
+    public class Schedule
+    {
+        public int ID { get; set; }
+    }
+}
