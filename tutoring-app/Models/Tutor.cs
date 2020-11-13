d9e7a8a37d0e561f2fd527af3051d7c6857dacb9
Thomas <thmsphung2000@gmail.com>
2020-11-13 14:02:31 -0800

    Adding user superclass model

--- tutoring-app/Models/Tutor.cs
+++ tutoring-app/Models/Tutor.cs
@@ -5,13 +5,8 @@ using System.Threading.Tasks;
 
 namespace tutoring_app.Models
 {
-    public class Tutor
+    public class Tutor : User
     {
-        public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public int Phone { get; set; }
-        public int Email { get; set; }
+
     }
 }
---
+++ tutoring-app/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tutoring_app.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int Phone { get; set; }
+        public int Email { get; set; }
+    }
+}
