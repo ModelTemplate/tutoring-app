9af15f0d42e10113b1fd796d0b15582d3bc3aa69
Thomas <thmsphung2000@gmail.com>
2020-11-24 22:39:56 -0800

    CreateIdentitySchema migration

--- tutoring-app/Startup.cs
+++ tutoring-app/Startup.cs
@@ -27,6 +27,7 @@ namespace tutoring_app
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // duplicated code in Areas/Identity/IdentityHostingStartup.cs
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
