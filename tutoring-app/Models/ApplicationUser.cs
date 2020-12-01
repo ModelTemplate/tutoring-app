1ea40ad5f731ce5e5f57afcfdaddbb8941160a39
Thomas <thmsphung2000@gmail.com>
2020-12-01 13:44:17 -0800

    Primary key is now type int

--- tutoring-app/Models/ApplicationUser.cs
+++ tutoring-app/Models/ApplicationUser.cs
@@ -10,7 +10,7 @@ namespace tutoring_app.Models
     /// <summary>
     /// Represents a user of this web app
     /// </summary>
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser<int>
     {
         public virtual Tutor Tutor { get; set; }
         // public virtual Student Student { get; set; }
